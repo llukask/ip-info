92def94ce71eef2b8961ef771bf1d4aa54f43127
Lukas Kilian <lukas@kilian.ltd>
2023-02-27 10:22:53 +0100

    move css to separate file + caching for css

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
-use std::env;
+use base64::{engine::general_purpose, Engine};
+use html_escape;
+use sha3::{
+    digest::{ExtendableOutput, Update},
+    Shake128,
+};
+use std::{env, include_str, io::Read};
 use tiny_http::{Header, Request, Response, Server};
 
-fn get_ip(req: &Request) -> String {
+const MAIN_CSS: &'static str = include_str!("main.css");
+
+fn get_real_ip(req: &Request) -> String {
     for header in req.headers() {
         if header.field.as_str() == "X-Real-Ip" {
             let ip = header.value.as_str();
@@ -16,7 +24,34 @@ fn get_ip(req: &Request) -> String {
     ip
 }
 
+fn compute_etag(data: &str) -> String {
+    let mut hasher = Shake128::default();
+    hasher.update(data.as_bytes());
+
+    let mut reader = hasher.finalize_xof();
+    let mut result = [0u8; 16];
+    reader.read(&mut result).expect("Could compute ETag");
+
+    format!(
+        "W/\"{hash}\"",
+        hash = general_purpose::STANDARD_NO_PAD.encode(result)
+    )
+}
+
+fn log_response<R: std::io::Read>(request: &Request, response: &Response<R>, real_ip: &str) {
+    println!(
+        "{ip} {method} {url} {status} {response_size}",
+        ip = real_ip,
+        method = request.method().as_str(),
+        url = request.url(),
+        status = response.status_code().0,
+        response_size = response.data_length().unwrap_or(0)
+    );
+}
+
 fn main() {
+    let css_etag = compute_etag(MAIN_CSS);
+
     let port: i32 = env::var("PORT")
         .ok()
         .and_then(|port| port.parse().ok())
@@ -30,129 +65,103 @@ fn main() {
     println!("server running @ {}", &endpoint);
 
     for request in server.incoming_requests() {
+        let real_ip = get_real_ip(&request);
+
         match request.url() {
             "/main.css" => {
-                let response = Response::from_string(
-                    r#"
-                *, *::before, *::after {
-                    box-sizing: border-box;
-                }
+                let etag_matches = request
+                    .headers()
+                    .iter()
+                    .find(|h| h.field.as_str().to_ascii_lowercase() == "if-none-match")
+                    .map(|h| h.value == css_etag)
+                    .unwrap_or(false);
 
-                * {
-                    margin: 0;
-                }
+                if etag_matches {
+                    let response = Response::empty(304)
+                        .with_header(
+                            Header::from_bytes(
+                                &b"Content-Type"[..],
+                                &b"text/css; charset=utf-8"[..],
+                            )
+                            .unwrap(),
+                        )
+                        .with_header(
+                            Header::from_bytes(&b"Cache-Control"[..], &b"max-age=86400"[..])
+                                .unwrap(),
+                        )
+                        .with_header(Header::from_bytes(&b"ETag"[..], css_etag.as_str()).unwrap());
 
-                html, body {
-                    height: 100%;
-                }
+                    log_response(&request, &response, &real_ip);
+                    request.respond(response);
+                } else {
+                    let response = Response::from_string(MAIN_CSS)
+                        .with_header(
+                            Header::from_bytes(
+                                &b"Content-Type"[..],
+                                &b"text/css; charset=utf-8"[..],
+                            )
+                            .unwrap(),
+                        )
+                        .with_header(
+                            Header::from_bytes(&b"Cache-Control"[..], &b"max-age=86400"[..])
+                                .unwrap(),
+                        )
+                        .with_header(Header::from_bytes(&b"ETag"[..], css_etag.as_str()).unwrap());
 
-                body {
-                    background-color: #292D3E;
-                    color: #959DCB;
-                    line-height: 1.5;
-                    font-family: monospace;
-                    font-size: 1.2rem;
-                }
-
-                header {
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    justify-content: center;
-                    height: 100%;
-                }
-
-                h1 {
-                    margin-block: 1rem;
-                    text-align: center;
-                    font-size: 2.5rem;
-                }
-
-                header > code {
-                    text-decoration: underline;
-                    font-size: 3rem;
-                }
-
-                main {
-                    display: flex;
-                    gap: 0.5rem;
-                    flex-direction: column;
-                    align-items: center;
-                    padding-block: 1rem;
-                }
-
-                .header-container {
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    max-width: 90vw;
-                }
-
-                code {
-                    max-width: min(80ch, 90vw);
-                    text-align: center;
-                    overflow-wrap: break-word;
-                }
-            "#,
-                )
-                .with_header(
-                    Header::from_bytes(&b"Content-Type"[..], &b"text/css; charset=utf-8"[..])
-                        .unwrap(),
-                );
-
-                #[allow(unused_must_use)]
-                {
+                    log_response(&request, &response, &real_ip);
                     request.respond(response);
                 }
             }
             "/" => {
-                println!(
-                    "received request! method: {:?}, url: {:?}, headers: {:?}, remote_addr: {}",
-                    request.method(),
-                    request.url(),
-                    request.headers(),
-                    request.remote_addr()
-                );
-
                 let mut response_body = String::new();
                 response_body.push_str(
                     format!(
                         r#"
-        <html>
-            <head>
-                <title>ip stats</title>
-                <link rel="stylesheet" href="main.css">
-            </head>
-            <body>
-                <header>
-                    <h1>your ip is:</h1>
-                    <code>{ip}</code>
-                </header>
-                <main>
+<html>
+    <head>
+        <title>ip stats</title>
+        <link rel="stylesheet" href="main.css">
+        <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon"> 
+    </head>
+    <body>
+        <header>
+            <h1>your ip is:</h1>
+            <code>{ip}</code>
+        </header>
+        <main>
         "#,
-                        ip = get_ip(&request)
+                        ip = &real_ip
                     )
                     .as_str(),
                 );
                 for header in request.headers() {
+                    let mut encoded_header_field = String::new();
+                    let mut encoded_header_value = String::new();
+
+                    html_escape::encode_safe_to_string(
+                        header.field.as_str(),
+                        &mut encoded_header_field,
+                    );
+                    html_escape::encode_safe_to_string(&header.value, &mut encoded_header_value);
+
                     response_body.push_str(
                         format!(
                             r#"        
-                    <div class="header-container">
-                        <code>[{}]</code>
-                        <code>{}</code>
-                    </div>
+            <div class="header-container">
+                <code>[{}]</code>
+                <code>{}</code>
+            </div>
                 "#,
-                            header.field, header.value
+                            &encoded_header_field, &encoded_header_value
                         )
                         .as_str(),
                     );
                 }
                 response_body.push_str(
                     r#"
-                </main>
-            </body>
-        </html>
+        </main>
+    </body>
+</html>
         "#,
                 );
 
@@ -160,23 +169,17 @@ fn main() {
                     Header::from_bytes(&b"Content-Type"[..], &b"text/html; charset=utf-8"[..])
                         .unwrap(),
                 );
-
-                #[allow(unused_must_use)]
-                {
-                    request.respond(response);
-                }
+                log_response(&request, &response, &real_ip);
+                request.respond(response);
             }
             _ => {
                 let response = Response::from_string("404 not found").with_header(
                     Header::from_bytes(&b"Content-Type"[..], &b"text/plain; charset=utf-8"[..])
                         .unwrap(),
                 );
-
-                #[allow(unused_must_use)]
-                {
-                    request.respond(response);
-                }
+                log_response(&request, &response, &real_ip);
+                request.respond(response);
             }
-        }
+        };
     }
 }
